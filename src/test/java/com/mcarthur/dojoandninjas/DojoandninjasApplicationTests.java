9ea42e8c34f7a0d56eeb9ebbe05080fe398c7b0d
McArthur Diaz <mc.arthur_d@hotmail.com>
2017-10-25 17:47:46 -0700

    setup

---
+++ src/test/java/com/mcarthur/dojoandninjas/DojoandninjasApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mcarthur.dojoandninjas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DojoandninjasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
